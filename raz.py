c454f6936387045e0a6df051868510f22dbe587c
mani <manijamali2003@gmail.com>
2022-05-12 20:58:44 +0430

    RAZ Source Code

--- raz.py
+++ raz.py
@@ -11,6 +11,7 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 
+
 class Key:
     # create keys for wallet or bank
     def __init__(self):
